069fc7a6d94c88e0218f2ac6f225950d6b006fc3
CallMeCJ_ <36345590+CallMeCJUnderscore@users.noreply.github.com>
2023-11-20 15:56:19 -0500

    Created child class (#1)

---
+++ src/main/java/com/pluralsight/Child.java
@@ -0,0 +1,22 @@
+package com.pluralsight;
+
+public class Child extends Person{
+    /*---------------VARIABLES---------------*/
+
+    private int schoolGrade;
+
+    /*--------------CONSTRUCTORS-------------*/
+
+    public Child(String firstName, String lastName, int age, int grade) {
+        super(firstName, lastName, age);
+        this.schoolGrade = grade;
+    }
+
+    /*------------GETTERS/SETTERS------------*/
+
+
+
+    /*---------------FUNCTIONS---------------*/
+
+
+}
